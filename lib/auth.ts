def41ddbfa01b9ecd7990a45e4b73143e5be2ca3
thinkter <ashmangamer0406@gmail.com>
2026-03-24 20:26:10 +0530

    a

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,4 @@
+//cant use experimental joins :( it breaks auth
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db";
